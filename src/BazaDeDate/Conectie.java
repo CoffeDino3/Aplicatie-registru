e2e216f3680481326ae9be1bbe47d0a188363b47
CoffeDino3 <andrei.chiochiu9@gmail.com>
2026-05-05 16:40:26 +0200

    Astazi am lucrat la partea cu SQL utilizand PostgrSQL unde am creat tabelele necesare si m-am conectat la baza de date creata in cod.

---
+++ src/BazaDeDate/Conectie.java
@@ -0,0 +1,14 @@
+package BazaDeDate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conectie {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Registru";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
